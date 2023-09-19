0534d20b3018b15b89dd140de284cb76a77bd118
Brendan Leal <leal.brendan@gmail.com>
2023-09-19 14:20:54 -0700

    initial implementation of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,27 @@
+import Image from "next/image"
+import SocialIcons from "./SocialIcons"
+
 export default function Footer() {
     return (
-        <footer>
-            <p>Stuff here</p>
+        <footer className="font-unna py-3">
+            <SocialIcons />
+            <div className="text-center py-4  flex flex-col">
+                <p>Site made possible by&nbsp;
+                    <a className="underline" href="https://astrolabstudio.com">Astrolab Studio
+                    </a>
+                    &nbsp;
+                    &nbsp;
+                    <Image
+                        src="/img/astrolab-logo.png"
+                        alt="astrolab logo"
+                        width={25}
+                        height={25}
+                        className="inline"
+                    />
+                </p>
+                <p>&copy; copyright 2023</p>
+            </div>
+
         </footer>
     )
 }
