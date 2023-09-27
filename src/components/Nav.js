9e0ddef297650eff417f26a80e81ec303c43e003
Brendan Leal <leal.brendan@gmail.com>
2023-09-27 14:34:57 -0700

    state management for toggling nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,22 @@
-export default function Nav() {
-    const showNav = () => {
+import Menu from "./Menu";
 
-    }
+export default function Nav({ menuOpen, setMenuOpen }) {
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    return (
-        // Mobile Nav
-        <div onClick={showNav} className="absolute z-10 right-0 pr-4 pt-12 flex flex-col items-end gap-1 md:hidden">
-            <div className="w-12 h-1 bg-off-white rounded-lg"></div>
-            <div className="w-6 h-1 bg-off-white rounded-lg"></div>
-        </div>
-    )
+  return (
+    <div>
+      <div
+        onClick={toggleMenu}
+        className={`absolute z-10 right-0 pr-4 pt-12 flex flex-col items-end gap-1 md:hidden ${
+          menuOpen ? "hidden" : ""
+        }`}
+      >
+        <div className="w-12 h-1 bg-off-white rounded-lg"></div>
+        <div className="w-6 h-1 bg-off-white rounded-lg"></div>
+      </div>
+      <Menu toggleMenu={toggleMenu} menuOpen={menuOpen} />
+    </div>
+  );
 }
