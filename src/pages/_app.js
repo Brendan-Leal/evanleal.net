dc1b7edfc31e3cd61e827b5afb4cc97d77dd09d7
Brendan Leal <leal.brendan@gmail.com>
2023-09-27 14:33:34 -0700

    add nav component with state layout & formatting

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,33 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Nav from "@/components/Nav";
+import { useState } from "react";
 
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 const sultan = localFont({
-  src: '../../public/fonts/Sultan.otf',
-  variable: '--font-sultan',
-})
+  src: "../../public/fonts/Sultan.otf",
+  variable: "--font-sultan",
+});
 
-import { Unna } from "next/font/google"
+import { Unna } from "next/font/google";
 const unna = Unna({
   subsets: ["latin"],
-  variable: '--font-unna',
+  variable: "--font-unna",
   weight: ["400", "700"],
-})
-
-
+});
 
 export default function App({ Component, pageProps }) {
-  return <main className={`${sultan.variable} ${unna.variable}`}>
-    <Component {...pageProps} />
-  </main>
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+      <Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <main
+        className={`${sultan.variable} ${unna.variable} ${
+          menuOpen ? "hidden" : null
+        }`}
+      >
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
