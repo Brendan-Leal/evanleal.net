da6bc9c0cfa31d39931c0ef0275311b539e8c93e
Brendan Leal <leal.brendan@gmail.com>
2023-09-27 16:58:44 -0700

    add navigation links to menu

---
+++ constants/navLink.js
@@ -0,0 +1,22 @@
+export const NAV_LINKS = [
+  {
+    name: "Home",
+    path: "/",
+    key: "nav_0",
+  },
+  {
+    name: "Existence Podcast",
+    path: "/podcast",
+    key: "nav_1",
+  },
+  {
+    name: "About",
+    path: "/about",
+    key: "nav_2",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+    key: "nav_3",
+  },
+];
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
+import { NAV_LINKS } from "../../constants/navLink";
+import SocialIcons from "./SocialIcons";
 
 export default function Menu({ toggleMenu, menuOpen }) {
   return (
     <div
       className={`${
         menuOpen ? "flex" : "hidden"
-      } bg-off-white w-screen h-screen text-cinema-black flex-col`}
+      } bg-off-white w-screen h-screen text-cinema-black flex-col justify-center`}
     >
-      <div onClick={toggleMenu} className="">
-        <Image src="/img/dismiss.png" width={40} height={40} alt="" />
+      <div
+        onClick={toggleMenu}
+        className="w-[40px] h-[40px] absolute right-4 top-4"
+      >
+        <Image src="/img/dismiss.png" alt="" fill={true} />
       </div>
-      <a>Home</a>
-      <a>Podcast</a>
+      <ul className="text-2xl font-unna text-center flex flex-col gap-14">
+        {NAV_LINKS.map((link) => (
+          <li key={link.key} onClick={toggleMenu}>
+            <Link href={`${link.path}`}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
