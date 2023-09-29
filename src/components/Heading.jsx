41ca5566f35de1771b93a7a294de16af1f72343b
Brendan Leal <leal.brendan@gmail.com>
2023-09-29 15:18:25 -0700

    implement responsive design for Heading component

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,23 +1,18 @@
 import Image from "next/image";
 
-export default function Heading({
-  headingText,
-  bgImg,
-  position,
-  alt,
-}) {
+export default function Heading({ headingText, bgImg, alt, imgPosition }) {
   return (
     <>
-      <div className="relative h-[33dvh] flex flex-col justify-end">
+      <div className="relative h-[33dvh] md:h-[50vh] flex flex-col justify-end md:justify-start md:pt-16">
         <div className="absolute z-10 w-screen h-full bg-gradient-to-t from-overlay-black opacity-50"></div>
         <Image
           src={bgImg}
           fill={true}
           objectFit="cover"
-          objectPosition={position}
+          className={imgPosition}
           alt={alt}
         />
-        <h1 className="text-5xl text-center font-sultan font-bold text-title-red drop-shadow-[2px_2px_3px_#EF9F20] tracking-[.5rem] z-20">
+        <h1 className="text-5xl text-center font-sultan font-bold text-title-red drop-shadow-[2px_2px_3px_#EF9F20] tracking-[.5rem] z-20 md:text-desktop-3xl">
           {headingText}
         </h1>
       </div>
