c08654d80b5c70e225a47a51acfbaf6021f45039
Brendan Leal <leal.brendan@gmail.com>
2023-10-01 15:14:55 -0700

    I have not idea why it works not it just does. reformatted (prettier)

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,26 +1,28 @@
-import Logo from "./Logo"
-import { logoData } from "../../constants/logoData"
+import Logo from "./Logo";
+import { logoData } from "../../constants/logoData";
 
 export default function SocialIcons({ vertical }) {
-    // Deep copy logoData
-    let logos = JSON.parse(JSON.stringify(logoData))
+  // Deep copy logoData
+  let logos = JSON.parse(JSON.stringify(logoData));
 
-    if (vertical) {
-        logos = logos.filter(data => {
-            return data.required
-        })
-    }
-    return (
-        <div
-            className={`flex ${vertical ? "flex-col" : "flex-row"} justify-around`}>
-            {logos.map(data => <Logo
-                key={data.key}
-                src={data.path}
-                alt={data.alt}
-                url={data.href}
-                style={data.tailwindCSS}
-            />)}
-        </div>
-    )
+  if (vertical) {
+    logos = logos.filter((data) => {
+      return data.required;
+    });
+  }
+  return (
+    <div
+      className={`flex ${vertical ? "flex-col" : "flex-row"} justify-around`}
+    >
+      {logos.map((data) => (
+        <Logo
+          key={data.key}
+          src={data.path}
+          alt={data.alt}
+          url={data.href}
+          style={data.tailwindCSS}
+        />
+      ))}
+    </div>
+  );
 }
-
