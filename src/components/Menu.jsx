fa2260cfa57b7a242fef50ea4d092b2e24791ab7
Brendan Leal <leal.brendan@gmail.com>
2023-10-01 17:11:07 -0700

    add social links to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,21 +8,26 @@ export default function Menu({ toggleMenu, menuOpen }) {
     <div
       className={`${
         menuOpen ? "flex" : "hidden"
-      } bg-off-white w-screen h-screen text-cinema-black flex-col justify-center`}
+      } bg-off-white w-screen h-screen text-cinema-black flex-col justify-between`}
     >
       <div
         onClick={toggleMenu}
-        className="w-[40px] h-[40px] absolute right-4 top-4"
+        className="absolute w-[40px] h-[40px] right-4 top-4"
       >
         <Image src="/img/dismiss.png" alt="" fill={true} />
       </div>
-      <ul className="text-2xl font-unna text-center flex flex-col gap-14">
+      {/* Page links */}
+      <ul className="text-2xl font-unna text-center flex flex-col gap-14 pt-[20dvh]">
         {NAV_LINKS.map((link) => (
           <li key={link.key} onClick={toggleMenu}>
             <Link href={`${link.path}`}>{link.name}</Link>
           </li>
         ))}
+        {/* Social Icon List */}
       </ul>
+      <div className="pb-8">
+        <SocialIcons dark="true" showFullSet={true} />
+      </div>
     </div>
   );
 }
