8232da6d53b12bc6c3c8b8688f65ade41db500ff
Brendan Leal <leal.brendan@gmail.com>
2023-10-11 19:52:07 -0700

    grid adjustments

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -21,29 +21,29 @@ export default function Podcast({ rssData }) {
         alt="Evan podcasting"
       />
 
-      <div className="grid grid-cols-2 lg:grid-cols-3 grid-rows-auto font-unna p-4 text-center gap-4 max-w-xl md:max-w-3xl mx-auto">
-        <p className="text-base col-span-full lg:text-left">
+      <div className="grid grid-cols-2 lg:grid-cols-4 grid-rows-auto font-unna p-4 text-center gap-4 lg:gap-5 max-w-xl md:max-w-3xl mx-auto lg:max-w-5xl lg:text-left">
+        <p className="text-base lg:text-desktop-base col-span-full lg:col-span-3">
           Conversations contemplating the essence of what it means to exist &
           pursue a life well-lived through discussions on love, death,
           philosophy & connection.
         </p>
 
-        <div className="w-full h-[1px] bg-off-white my-6 "></div>
+        <div className="hidden lg:block w-full h-[1px] bg-off-white col-span-3"></div>
 
-        <div className="col-span-full">
-          <h2 className="text-2xl">Latest Episode</h2>
+        <div className="col-span-full lg:col-span-1 lg:row-start-3">
+          <h2 className="text-2xl lg:hidden">Latest Episode</h2>
 
-          <div className="w-full h-[25vh] relative flex flex-col justify-end">
+          <div className="w-full h-[25vh] lg:h-full relative flex flex-col justify-end">
             <a href={rssData.href} target="_blank">
               <Image
                 src="/img/pod-in-park.png"
                 fill
                 objectFit="cover"
-                objectPosition="bottom"
+                objectPosition="bottom right"
                 alt="existence podcast cover art"
               />
             </a>
-            <p className="text-3xl font-sultan font-bold text-title-red drop-shadow-[2px_2px_3px_#EF9F20] tracking-[1rem] absolute top-[10%] left-0 right-0 ">
+            <p className="text-3xl lg:text-desktop-lg font-sultan font-bold text-title-red drop-shadow-[2px_2px_3px_#EF9F20] tracking-[1rem] lg:tracking-[.5rem] absolute top-[10%] left-0 right-0 mx-auto text-center">
               Existence
             </p>
 
@@ -52,42 +52,56 @@ export default function Podcast({ rssData }) {
             </div>
           </div>
         </div>
-        <h2 className="text-2xl col-span-full">Featured Episodes</h2>
 
-        <div className="relative w-full pt-[50%] ">
+        <div className="hidden lg:block col-span-2 text-left">
+          <h2 className="text-desktop-xl">Latest Episode:</h2>
+          <p className="text-base text-left">
+            {rssData.date} | {rssData.title}
+          </p>
+          <br />
+          <p className="text-base col-span-2">{rssData.description}</p>
+        </div>
+
+        {/* FIXME: Only 3 icons here */}
+        <SocialIcons vertical />
+        <h2 className="text-2xl col-span-full lg:hidden">Featured Episodes</h2>
+
+        <div className="relative w-full pt-[50%] lg:col-start-1 lg:col-end-3">
           <Image
             src="/img/sn-2.png"
             fill
             objectFit="cover"
-            alt=""
+            alt="thumbnail season 2 trailer"
             className="w-full h-full top-0 left-0 object-cover"
           />
         </div>
 
-        <div className="w-full relative pt-[50%]">
+        <div className="w-full relative pt-[50%] lg:col-start-3 lg:col-end-5">
           <Image
             src="/img/healthy-relationships.png"
-            alt="profile"
+            alt="thumbnail healthy relationships episode"
             objectFit="cover"
+            objectPosition="top"
             fill
             className="w-full h-full top-0 left-0 object-cover"
           />
         </div>
-        <div className="w-full relative pt-[50%]">
+        <div className="w-full relative pt-[50%] lg:col-start-1 lg:col-end-3">
           <Image
             src="/img/dd-laying.png"
             fill
             objectFit="cover"
-            alt=""
+            alt="thumbnail death & disability"
             className="w-full h-full top-0 left-0 object-cover"
           />
         </div>
-        <div className="w-full relative pt-[50%]">
+        <div className="w-full relative pt-[50%] lg:col-start-3 lg:col-end-5">
           <Image
             src="/img/controversial-opinions.png"
             fill
             objectFit="cover"
-            alt=""
+            objectPosition="top"
+            alt="thumbnail controversial opinions episode"
             className="w-full h-full top-0 left-0 object-cover"
           />
         </div>
