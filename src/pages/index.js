4184161c45d1b9cbd7bcaca374850451e559f758
Brendan Leal <leal.brendan@gmail.com>
2023-10-13 14:13:49 -0700

    add social icons on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { useContext } from "react";
 import { MenuContext } from "@/pages/_app";
 import { Transition } from "@headlessui/react";
 import { TRANSITION_TIME_CSS } from "../../constants/transitionTime";
+import SocialIcons from "@/components/SocialIcons";
 
 export default function Home() {
   const menu = useContext(MenuContext);
@@ -14,25 +15,26 @@ export default function Home() {
       <Head>
         <title>Home</title>
       </Head>
-        <div className="relative">
-          <Hero />
-          <video className="w-full h-[100dvh]">
-            <source src="/videos/mobile-trailer.mp4" />
-          </video>
+      <div className="relative">
+        <Hero />
+        <video className="w-full h-[100dvh]">
+          <source src="/videos/mobile-trailer.mp4" />
+        </video>
 
-          {/* Quote  */}
-          <div className="font-unna text-base text-center flex items-center justify-center gap-2 my-6 px-12">
-            <div className="w-[2px] h-[100px] bg-off-white "></div>
-            <div className="flex-col">
-              <p>
-                &quot;The mystery of human existence lies not in just staying
-                alive, but in finding something to live for.&quot;
-              </p>
-              <p>- Fyodor Dostoyevsky</p>
-            </div>
+        {/* Quote  */}
+        <div className="font-unna text-base text-center flex items-center justify-center gap-2 my-6 px-12">
+          <div className="w-[2px] h-[100px] bg-off-white "></div>
+          <div className="flex-col">
+            <p>
+              &quot;The mystery of human existence lies not in just staying
+              alive, but in finding something to live for.&quot;
+            </p>
+            <p>- Fyodor Dostoyevsky</p>
           </div>
         </div>
-        <Footer />
+      </div>
+      <SocialIcons />
+      <Footer />
     </>
   );
 }
