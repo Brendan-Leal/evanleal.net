81a0c19afe5db037b0ef8675fb28fd770318024d
Brendan Leal <leal.brendan@gmail.com>
2023-10-16 10:06:31 -0700

    add event listeners to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,23 @@
 import Image from "next/image";
 
-export default function Logo({ src, alt, url, size = 50, className = null,  }) {
+export default function Logo({
+  src,
+  alt,
+  url,
+  onMouseEnter,
+  onMouseLeave,
+  size = 50,
+  className = null,
+}) {
   return (
     <>
-      <div className={className}>
+      <div
+        className={className}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+      >
         <a href={url} target="_blank">
-          <Image src={src} width={size} height={size} alt={alt} />
+          <Image src={src} width={size} height={size} alt={alt} priority />
         </a>
       </div>
     </>
