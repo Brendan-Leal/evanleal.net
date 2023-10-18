02a3b7566ba90fc3f5ee6119dd8ac7ffedb5815d
Brendan Leal <leal.brendan@gmail.com>
2023-10-17 19:13:29 -0700

    implement component with grid

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,40 @@
+import Image from "next/image";
+
 export default function Hero() {
   return (
     <>
-      <div className="relative flex flex-col items-center bg-homepage-hero-mobile md:bg-homepage-hero bg-no-repeat bg-[40%_bottom] md:bg-right lg:bg-[95%_bottom] bg-cover h-screen">
+      <div className="h-[100dvh] relative grid grid-cols-1 grid-rows-4">
+        <Image
+          src="/img/pod-in-park.png"
+          fill
+          sizes="100vw"
+          className="hidden object-cover object-right-bottom md:block"
+          quality={100}
+          priority
+          alt="Evan outside podcasting in the park"
+        />
+
+        <Image
+          src="/img/pod-in-park-mobile.png"
+          fill
+          sizes="100vw"
+          className="object-cover object-bottom md:hidden"
+          quality={100}
+          priority
+          alt="Evan outside podcasting in the park"
+        />
+
         <div className="z-0 w-full h-screen absolute bg-gradient-to-b from-overlay-black top-0 opacity-[0.60]"></div>
 
-        <h1 className="relative text-5xl text-center pt-16 md:text-desktop-3xl font-sultan font-bold text-title-red drop-shadow-[2px_2px_3px_#EF9F20] tracking-[1rem] lg:tracking-[5rem]">
-          Evan
-          <br />
-          Leal
-        </h1>
-        <p className="relative font-unna text-xl md:text-desktop-xl">
+        <div className="flex flex-col justify-start items-center row-start-2">
+          <h1 className="text-5xl md:text-desktop-3xl font-sultan font-bold text-title-red drop-shadow-[2px_2px_3px_#EF9F20] tracking-[2rem] lg:tracking-[2.5rem] ">
+            Evan <br className="md:hidden" /> Leal
+          </h1>
+        </div>
+
+        <p className="relative font-unna text-xl md:text-desktop-xl row-start-3 mx-auto">
           Podcast Host. Producer.
         </p>
-        <img
-          src="/img/chevron-down-solid.svg"
-          alt=""
-          className="w-8 h-8 absolute bottom-2"
-        />
       </div>
     </>
   );
